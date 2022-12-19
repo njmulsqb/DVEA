fcf9078b2222db4ae4b06634dc748199ff58caaf
Naja <administrator@CRIBL-SAQICNAJ2.local>
2022-12-19 15:03:25 +0500

    init electron with basic configuration

---
+++ main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron');
+try {
+	require('electron-reloader')(module);
+} catch {}
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences:{
+        nodeIntegration: true,
+      }
+  }
+  );
+
+  win.loadFile('index.html');
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
